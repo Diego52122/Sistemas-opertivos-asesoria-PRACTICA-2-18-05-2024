53734aad1e5761456f7a2b73cf87cc0fa69185c3
Diego52122 <a340401995@my.uvm.edu.mx>
2024-05-18 21:21:52 -0600

    PRACTICA DE BASE Y ALTURA
    PRACTICA DE BASE Y ALTURA ASESORÍA SISTEMAS OPERATIVOS

---
+++ BASE Y ALTURA.py
@@ -0,0 +1,13 @@
+def calcular_area_rectangulo(base, altura):
+    area = base * altura
+    return area
+
+# Valores dados en el programa principal
+base = 5
+altura = 10
+
+# Calcula el área del rectángulo
+area_rectangulo = calcular_area_rectangulo(base, altura)
+
+# Imprime el resultado
+print("El área del rectángulo con base", base, "y altura", altura, "es:", area_rectangulo)
